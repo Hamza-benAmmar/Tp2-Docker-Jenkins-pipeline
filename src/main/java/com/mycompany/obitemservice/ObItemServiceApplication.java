073b0982b4e8addcec33dbec1734db8c48dc0f28
Hamza-BenAmmar <hamza.benammar@insat.ucar.tn>
2024-02-29 17:42:26 +0100

    modifed

--- src/main/java/com/mycompany/obitemservice/ObItemServiceApplication.java
+++ src/main/java/com/mycompany/obitemservice/ObItemServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ObItemServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(ObItemServiceApplication.class, args);
 	}
 
